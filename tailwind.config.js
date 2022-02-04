9ee4cf4c56a38695a6f90c589d82b9f66c6b1775
habibi55 <muhamadhabibi14168@gmail.com>
2022-02-04 23:15:40 +0700

    malem

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,26 @@
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ["Open Sans", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        luxspace: {
+          default: "#f9cada",
+          50: "#fffcfd",
+          100: "#fefafb",
+          200: "#fef2f6",
+          300: "#fdeaf0",
+          400: "#fbdae5",
+          500: "#f9cada",
+          600: "#e0b6c4",
+          700: "#bb98a4",
+          800: "#957983",
+          900: "#7a636b",
+        },
+      },
+    },
   },
   plugins: [],
 };
